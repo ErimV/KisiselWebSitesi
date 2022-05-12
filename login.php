76f592f907e156e5bc0ac5fcdc5344a262c1cc03
Erim Vurucu <55758293+ErimV@users.noreply.github.com>
2022-05-12 14:27:37 +0300

    Sayfa taslakları eklendi.

--- login.php
+++ login.php
@@ -1,14 +1,17 @@
 <?php
+	session_start();
+
 		if(isset($_POST['login'])){
 			$mail = $_POST['email'];
 			$pass = $_POST['password'];
 			if(($mail == "b211210355@sakarya.edu.tr") && $pass == "b211210355") {
+				$_SESSION['user'] = true;
 				echo 'Hoşgeldiniz ',$pass;
 				echo '<a href="index.html">Geri Dön</a>';
 			}
 			else {
 				echo 'Yanlış giriş yapıldı. Geri dönüp tekrar deneyiniz.';
-				echo '<a href="index.html">Geri Dön</a>';
+				echo '<a href="login.html"><br>Geri Dön</a>';
 			}
 		}
 
